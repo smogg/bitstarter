336f0ffa4d2d35281df98a16411fc9baec2c986c
Ubuntu <ubuntu@ip-172-31-24-204.us-west-2.compute.internal>
2013-07-13 12:24:30 +0000

    buffer test

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var f = fs.readFileSync("index.html");
+  var text = f.toString();
+  response.send(f);
 });
 
 var port = process.env.PORT || 5000;
